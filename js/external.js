6c7cebe5b2996501e7187c3ad64c8a99799933df
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-10 14:47:11 -0600

    Finished javascript exercise

---
+++ js/external.js
@@ -0,0 +1,43 @@
+"use strict"
+// console.log("Hello from internal JavaScript".)
+// console.log("Hello from external JavaScript".)
+
+// alert("Welcome to my Website!");
+// console.log(alert)
+// let userInput = prompt('What is your favorite color?');
+// alert(userInput + (' is my favorite color too!'));
+
+// let price = prompt(`How much did you pay for the movie?`)
+// let days = prompt('How many days did you rent the movie?');
+// alert(price * days)
+// console.log(alert)
+
+// let googlePay = parseFloat(prompt ('How much does Google pay you per hour?'));
+// let googleHours = parseFloat(prompt ('How many hours did you work for Google?'));
+// let alert = googlePay * googleHours
+// console.log(alert)
+//
+// let hours = prompt('How many hours did you work?');
+// let hourlyPay = prompt('What is your hourly pay?')
+// alert(hours * hourlyPay)
+// console.log(alert);
+
+// let maxStudents =true
+// let availability =true
+// let enroll = maxStudents && availability;
+//     prompt('Are you able to attend?')
+// if (enroll) {
+//     alert("The student can enroll in the class.");
+// } else {
+//     alert("The student cannot enroll in the class.");
+// }
+
+// let expiration = confirm('Is you coupon valid?');
+// let offer = prompt('How many items are you buying?')
+// let members = prompt(`Are you member?`)
+// let offerApply = offer >=2 && expiration;
+// if (offerApply) {
+//     alert("Coupon applied");
+// } else {
+//     alert("Coupon NOT applied");
+// }
---
+++ js/javascript-with-html.js
@@ -0,0 +1,5 @@
+"use strict";
+alert("Hello from javascript");
+// let didConfirm = confirm ("Would you like to confirm?")
+// let userName = prompt("What is your name?");
+// console.log('
