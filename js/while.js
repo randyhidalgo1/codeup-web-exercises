3d0538f557e51c2e39b6f661af87a4c19782e7e8
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-15 23:08:19 -0600

    Working on While Exercise

--- js/while.js
+++ js/while.js
@@ -1,11 +1,6 @@
 
-// Define a variable to keep track of the current value
 let value = 2;
-
-// Set the loop condition to keep going while the value is less than or equal to 65536
 while (value <= 65536) {
-    // Output the current value
     console.log(value);
-
     value *= 2;
 }
