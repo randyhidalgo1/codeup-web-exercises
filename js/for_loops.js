d0cd1b94ae9f4def0d5ae557a6ba0c0e5a96fbb0
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-15 21:47:41 -0600

    Completed Loops Exercise

---
+++ js/for_loops.js
@@ -0,0 +1,54 @@
+//
+// const showMultiplicationTable=(num)=> {
+//     for (let i = 1; i < 11; i = i +1){
+//     console.log(`${num} * ${i}= ${num * i}`)
+//     }
+// }
+// (showMultiplicationTable(7))
+
+// function randomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min+ 10 + 1) + min);
+// }
+// for (let i = 1; i <=10; i = i + 1){
+//     let myRandomNum = randomNumber(20, 200)
+//     // console.log(myRandomNum)
+//     if (myRandomNum % 2 ===0){ //if is even
+//         let output = `${myRandomNum} is even`
+//         console.log(output)
+//     } else { // else is odd
+//         let output = `${myRandomNum} is odd`
+//         console.log(output)
+//     }
+// }
+
+
+// const randomNumber = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+//
+// for (let i = 21; i < 201; i++) {
+//     console.log(randomNumber(21, 201));
+// }
+
+
+// function randomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+//
+// for (let i = 21; i <= 201; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(randomNumber(i, 201));
+//     }
+// }
+
+// for (let i = 1; i <= 9; i++) {
+//     let output = '';
+//     for (let j = 1; j <= i; j++) {
+//         output += i;
+//     }
+//     console.log(output);
+// }
+
+//for (let i = 100; i >= 5; i -= 5) {
+//    console.log(i);
+//}
--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,10 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 if (typeof userNumber === "string" || isNaN(userNumber) !== false) {
     alert(`That is not a number!`)
+
+
+//Random Function
+    function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min+ 10 + 1) + min);
+    }
+}
