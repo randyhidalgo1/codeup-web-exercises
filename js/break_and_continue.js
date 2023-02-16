c1d478d49886e93d047e09bae67eee2f863bdeb6
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-16 14:01:08 -0600

    Completed do-while Exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,35 +1,45 @@
- // let input;{}
- // while (true)   {
- //        let input = prompt("Enter odd number between 1-50:");
- //        console.log(input);
- //        if (input >= 1 && input <= 50 && input % 2 !== 1) {
- //           // if (input % 2 === 1 && input >= 1 && input <=50) {
- //            break;
- //        }
- //        // console.log("Skip number: " + input);
- //        for (let i = 1; i <= 50; i += 1) {
- //            if (i == input) {
- //                console.log("Skip number: " + input);
- //                continue;
- //            }
- //            console.log("This an odd number: " + i);
- //        }
- //    }
+//  let input;{
+//      while (true)   {
+//          let input = prompt("Enter odd number between 1-50:");
+//          //console.log(input);
+//          //if (input % 2 === 1 && input >= 1 && input <=50){
+//          //if (input >= 1 && input <= 50 && input % 2 !== 0) {
+//           if (input % 2 === 1 && input >= 1 && input <=50) {
+//              break;
+//          }
+// }
+//         console.log("Skip number: " + input);
+//         for (let i = 1; i <= 50; i += 1) {
+//             if (i === input) {
+//                 console.log("Skip number: " + input);
+//                 continue;
+//             }
+//             console.log("This an odd number: " + input);
+//         }
+//     }
 
 //Example have photo
-// let userprompt = prompt ('Please enter number between 1-50')
-//  let userpromptNum; = parseFloat((userprompt);
-//     while (userpromptNum) = % 2 === 1 && userpromptNum >= 1 && userpromptNum <=50){{
-//         userprompt = prompt('Enter an Odd number between 1-50')
-//         userpromptNum = % 2 === 1 && userpromptNum >= 1 && userpromptNum <=50){
-//             break;
-//         }
-//
+let userPrompt = prompt ('Please enter number between 1-50')
+let userPromptNum; parseFloat(userPrompt);{
+while(userPromptNum % 2 === 1 && userPromptNum >= 1 && userPromptNum <=50){
+userPrompt = prompt(`Please enter odd number 1-50`);
+userPromptNum = parseInt(userPrompt);
+}
+for (let i = 1; i <=50; i = i + 2){
+    if (i === userPromptNum){
+        console.log(`Yikes! skipped #: ${i}`);
+        //continue
+    } else {
+        console.log(`This is odd ${i}`);
+
+
+//     let (userPromptNum === % 2 = 1 && userPromptNum >= 1 && userPromptNum <= 50){
+//         break;
 //     }
+// }
 //     for (let i = 1, i <= 50; i = i + 2){
 //         if (i === userpromptNum) {
 //             console.log('yikes!${i)');
 //             continue;
 //         }
 //         console.log(` odd number${i}`)
-//  }
--- js/do-while.js
+++ js/do-while.js
@@ -1,9 +1,25 @@
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-for (let i = 1; i <=1; i = i + 1){
-    let myRandomNum = randomNumber(1, 5)
-    //console.log(myRandomNum)
-    let output = `${myRandomNum} cones sold`
-    console.log(output)
-}
+// function randomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// for (let i = 1; i <=1; i = i + 1){
+//     let myRandomNum = randomNumber(1, 5)
+//     //console.log(myRandomNum)
+//     let output = `${myRandomNum} cones sold`
+//     console.log(output)
+// }
+
+let conesInventory = randomNumber(50, 100);
+do {
+    let conesWanted = randomNumber(1, 5);
+    if (conesWanted > conesInventory){
+        console.log(`Cannot sell you ${conesWanted} I only have ${conesInventory}`);
+        console.log(`{conesInventory} sold...`);
+        conesInventory = 0;
+    } else {
+    console.log(`${conesWanted} sold');
+       conesInventory = conesInventory - conesWanted;
+       }
+       console.log(`${conesInventory} left`);
+       } while(conesInventory > 0);
+       console.log(`sold them all`);
+ }
