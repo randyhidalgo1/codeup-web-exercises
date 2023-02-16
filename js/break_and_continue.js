bb7e3a066b336b58f2439b50e079ed279bf18272
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-16 09:46:35 -0600

    Working on Break-Contiue Exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,17 +1,16 @@
-
-let userInput;{}
-while (true) {
-    let userInput = prompt("Enter odd number between 1-50:");
-    console.log(userInput);
-
-    if (userInput >= 1 && userInput <= 50 && userInput % 2 !== 0) {
-        break;
-    } console.log("Skip number: " + userInput);
-}
-for (let i = 1; i <= 50; i += 2) {
-    if (i === userInput) {
-        console.log("Skip number: " + userInput);
-        continue;
+ let input;{}
+    while (true) {
+        let input = prompt("Enter odd number between 1-50:");
+        console.log(input);
+        if (input >= 1 && input <= 50 && input % 2 !== 1) {
+            break;
+        }
+        // console.log("Skip number: " + input);
+        for (let i = 1; i <= 50; i += 1) {
+            if (i == input) {
+                console.log("Skip number: " + input);
+                continue;
+            }
+            console.log("This an odd number: " + i);
+        }
     }
-    console.log("Odd number: " + i);
-}
