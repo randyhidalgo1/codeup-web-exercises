856460bd333834acc73f9860cc34328492980adc
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-02-27 22:36:53 -0600

    MY Examples

--- css/examples.css.js
+++ css/examples.css.js
@@ -1,7 +1,9 @@
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;1,100&display=swap');
 
 * {
+    background-image: url(/images/pexels-henry-&-co-1939485.jpg);
     box-sizing: border-box;
+    background-color: floralwhite;
 }
 
 img {
@@ -10,21 +12,16 @@ img {
     width: 100%;
 }
 
-/*.example {*/
-/*    background-color: red;*/
-/*    width:  400px;*/
-/*    height: 400px;*/
-/*    padding: 20px;*/
-/*}*/
-
 body {
     margin: 0px;
     padding:20px;
     font-family: Roboto, sans-serif;
 }
 
-h1, h2 {
-    font-family: sans-serif;
+h1, h2, h3 {
+    font-family: Roboto, sans-serif;
+    color: green;
+    text-decoration: underline;
 }
 
 .header1 {
@@ -33,66 +30,251 @@ h1, h2 {
     margin-bottom: 0px;
     transition: all ease 5s;
     font-weight: 100;
-    /*text-align: center;*/
-    /*color: purple;*/
+    text-align: center;
+    color: purple;
     /*transition: all ease .3s;*/
 }
 
-.header1:hover {
-    color: red;
-}
+/*.header1:hover {*/
+/*    color: red;*/
+/*}*/
 
 .text-center {
     text-align: center;
 }
 
-header p {
-    color: #4d4d4d;
-}
+/*header p {*/
+/*    color: #4d4d4d;*/
+/*}*/
 
-header {
+/*header {*/
+/*    padding: 20px;*/
+/*    border-bottom: 1px solid #000000;*/
+/*}*/
 
-    padding: 20px;
-    border-bottom: 1px solid #000000;
-}
-
-main {
-    padding: 20px;
-}
+/*main {*/
+/*    padding: 50px;*/
+/*}*/
 .features-container {
     display: flex;
     justify-content: space-between;
     border-bottom: 1px solid #000;
+    /*background-image: -ms-radial-gradient(circle + initial);*/
 }
 
-.photos-wrapper {
-    display:flex;
-    max-width: 100vw;
-}
-.photo-wrapper{
-    flex: 1 0;
-    min-width: 1;
-    aspect-ratio: 4/3;
-}
+/*.photos-wrapper {*/
+/*    display:flex;*/
+/*    max-width: 100vw;*/
+/*}*/
+/*.photo-wrapper{*/
+/*    flex: 1 0;*/
+/*    min-width: 1;*/
+/*    aspect-ratio: 4/3;*/
+/*}*/
 
-.photo-wrapper img {
-    transition: all ease .3s;
-}
+/*.photo-wrapper img {*/
+/*    transition: all ease .3s;*/
+/*}*/
 
-.photo-wrapper:hover {
-    transform: scale(1.1);
-}
+/*.photo-wrapper:hover {*/
+/*    transform: scale(1.1);*/
+/*}*/
 
-.photos-wrapper .photo-wrapper img {
-    aspect-ratio: 4/3;
-    object-fit: cover;
+/*.photos-wrapper .photo-wrapper img {*/
+/*    aspect-ratio: 4/3;*/
+/*    object-fit: cover;*/
 
-}
+/*}*/
 
-[data-features] {
-    opacity .5;
-}
+/*[data-features] {*/
+/*    opacity .5;*/
+/*}*/
 
 .features-container ul li:nth-child(2) {
-    color: red;
+    color: blue;
 }
+
+.tables {
+    border: 1px solid black;
+    padding: 1px;
+    border-spacing: 1px;
+    /*border-collapse: collapse;*/
+    font-family: "American Typewriter", sans-serif;
+    font-size: 10px;
+}
+
+
+.a:hover {
+    color: red; /* sets the color of links when the mouse is over them */
+    text-decoration: underline; /* adds underline to links when the mouse is over them */
+}
+
+.a:active {
+    color: green; /* sets the color of links when they are being clicked */
+}
+
+.image-container {
+    display: flex;
+    flex: 1 1 25%;
+    justify-content: left;
+    align-items: normal; /* vertically centers the images */
+    flex-wrap: wrap; /* allows the images to wrap to a new line if necessary */
+    max-width: 25%;
+    child-align: right;
+    margin: 10px;
+}
+
+/*.image-container img {*/
+/*    flex: 1 1 25%; !* makes the images 25% of the container width and height *!*!*/
+/*    margin: 10px; !* adds 10 pixels of margin around each image *!*/
+/*    max-width: 25%; !* ensures the images don't exceed their original size *!*/
+/*}*/
+
+
+
+/*Center the main heading*/
+/*Add a style to change the color of the headers on your page and make them underlined.*/
+/*Add a style to change the background color of the page.*/
+/*Add a style to add a background image to your background. Search how to manipulate the position, sizing of this background image.*/
+/*Add a style to change the list-style to something other than the default bullet.*/
+/*Add a style to change the font for the entire page. Use google fonts if you would like.*/
+/*Add a style to change the table, table cells td and rows tr padding and font sizes.*/
+/*Add a style to alter the anchor tags and its different states. (e.g. link, visited, hover and active).*/
+/*Continue experimenting with styles to increase the visual appeal of the page.*/
+/*Finally, commit the changes to your git repository, and push to GitHub.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;1,100&display=swap');
+//
+// * {
+//     box-sizing: border-box;
+// }
+//
+// img {
+//     display: block;
+//     max-width: 100%;
+//     width: 100%;
+// }
+//
+// /*.example {*/
+// /*    background-color: red;*/
+// /*    width:  400px;*/
+// /*    height: 400px;*/
+// /*    padding: 20px;*/
+// /*}*/
+//
+// body {
+//     margin: 0px;
+//     padding:20px;
+//     font-family: Roboto, sans-serif;
+// }
+//
+// h1, h2 {
+//     font-family: sans-serif;
+// }
+//
+// .header1 {
+//     font-size: 50px;
+//     background: #f1f1f1;
+//     margin-bottom: 0px;
+//     transition: all ease 5s;
+//     font-weight: 100;
+//     /*text-align: center;*/
+//     /*color: purple;*/
+//     /*transition: all ease .3s;*/
+// }
+//
+// .header1:hover {
+//     color: red;
+// }
+//
+// .text-center {
+//     text-align: center;
+// }
+//
+// header p {
+//     color: #4d4d4d;
+// }
+//
+// header {
+//
+//     padding: 20px;
+//     border-bottom: 1px solid #000000;
+// }
+//
+// main {
+//     padding: 20px;
+// }
+// .features-container {
+//     display: flex;
+//     justify-content: space-between;
+//     border-bottom: 1px solid #000;
+// }
+//
+// .photos-wrapper {
+//     display:flex;
+//     max-width: 100vw;
+// }
+// .photo-wrapper{
+//     flex: 1 0;
+//     min-width: 1;
+//     aspect-ratio: 4/3;
+// }
+//
+// .photo-wrapper img {
+//     transition: all ease .3s;
+// }
+//
+// .photo-wrapper:hover {
+//     transform: scale(1.1);
+// }
+//
+// .photos-wrapper .photo-wrapper img {
+//     aspect-ratio: 4/3;
+//     object-fit: cover;
+//
+// }
+//
+// [data-features] {
+//     opacity .5;
+// }
+//
+// .features-container ul li:nth-child(2) {
+//     color: red;
+// }
