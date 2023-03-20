ec729af9578b56ef15b12e7f7851dc0ed8a31c99
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-03-20 17:00:47 -0500

    jquery-national-parks

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -11,3 +11,17 @@
 $('dt').on('click', function(){
     $(this).next().fadeToggle();
 });
+
+$('button').on('click', function(){
+    $(this).next().fadeToggle();
+    $( "li" ).last().css( "background-color", "yellow" );
+
+});
+
+
+// $('button').on('click', function(){
+//     $(this).next().fontWeight(900);
+// });
+
+
+// $( "li" ).last("li").css( "background-color", "yellow" );
