0f65906e0fc469c816e650cc5b1f3ae76137871f
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-03-22 16:59:29 -0500

    mapbox-maps-api-exercise

--- js/mapbox-maps.js
+++ js/mapbox-maps.js
@@ -16,9 +16,9 @@ document.getElementById("setMarkerButton")
     })
 });
 const restaurants = [
-    {name: "JPrime", longitude: -97.7431, latitude: 30.2672, info: "Great Steaks!"},
-    {name: "Hopdoddy", longitude: -92.7431, latitude: 32.2672, info: "Great Burgers!"},
-    {name: "Franklin Barbecue", longitude: -96.7431, latitude: 35.2672, info: "Great BBQ!"},
+    {name: "JPrime", longitude: -98.7431, latitude: 29.2672, info: "Great Steaks!"},
+    {name: "Hopdoddy", longitude: -98.5831, latitude: 29.4563, info: "Great Burgers!"},
+    {name: "Franklin Barbecue", longitude: -98.6382, latitude: 29.2342, info: "Great BBQ!"},
 ]
 restaurants.forEach(restaurant=>{
     const marker = new mapboxgl.Marker()
@@ -31,36 +31,3 @@ restaurants.forEach(restaurant=>{
                 `);
     marker.setPopup(popup);
 });
-
-
-
-
-
-
-
-//
-// const map = L.mapbox
-//     .map('map')
-//     .setView([40, -74.5], 9)
-//     .addLayer(
-//         L.mapbox.styleLayer(
-//             'mapbox://styles/mapbox/streets-v12'
-//         )
-//     );
-//
-// restaurants.features.forEach(function (store, i) {
-//     restaurants.properties.id = i;
-// });
-// map.on('load', () => {
-//     /* Add the data to your map as a layer */
-//     map.addLayer({
-//         id: 'locations',
-//         type: 'circle',
-//         /* Add a GeoJSON source containing place coordinates and information. */
-//         source: {restaurants/GeoJSON},
-//             type: 'point',
-//             data: restaurants
-//         }
-//     });
-//
-
